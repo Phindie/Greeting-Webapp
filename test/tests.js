b85152b1eca16d74b5e29b105cd20dcf2e276630
Phindi <pn8256@gmail.com>
2018-09-10 10:33:52 +0200

    Modifications

--- test/tests.js
+++ test/tests.js
@@ -1,6 +1,6 @@
 
 let assert = require("assert");
-const services = require('../database');
+const Services = require('../database');
 const pg = require("pg");
 const Pool = pg.Pool;
 
@@ -22,65 +22,65 @@ describe('Greeting App test for both front-end and back-ends', function() {
     await pool.query('Delete from greet');
   });
 it('should return empty users data ', async function() {
-    var Services = services(pool);
-    let results = await Services.selectAlldata();
+    var services = Services(pool);
+    let results = await services.selectAlldata();
     assert.strictEqual(results.length, 0);
   });
 
 it('Should return the greet in English ', async function() {
-    var Services = services(pool);
-    await Services.addUsersOrIncrement('Phindi', 'Hello');
-    let results = await Services.selectAlldata();
+    var services = Services(pool);
+    await services.addUsersOrIncrement('Phindi', 'Hello');
+    let results = await services.selectAlldata();
     assert.strictEqual(results.length, 1);
   });
 
 it('Should return the greet in isiXhosa', async function() {
-    var Services = services(pool);
-    await Services.addUsersOrIncrement('Amanda', 'Molo');
-    let results = await Services.selectAlldata();
+    var services = Services(pool);
+    await services.addUsersOrIncrement('Amanda', 'Molo');
+    let results = await services.selectAlldata();
     assert.strictEqual(results.length, 1);
   });
 
 it('Should return the greet in Afrikaans', async function() {
-    var Services = services(pool);
-    await Services.addUsersOrIncrement('Amanda', 'Goeie Dag');
-    let results = await Services.selectAlldata();
+    var services = Services(pool);
+    await services.addUsersOrIncrement('Amanda', 'Goeie Dag');
+    let results = await services.selectAlldata();
     assert.strictEqual(results.length, 1);
   });
 
 it('Should return the insert value of people', async function() {
-    var Services = services(pool);
-    await Services.addUsersOrIncrement('Phindi', 'Goeie Dag');
-    await Services.addUsersOrIncrement('Zee', 'Molo');
-    await Services.addUsersOrIncrement('Amanda', 'Molo');
-    let results = await Services.selectAlldata();
+    var services = Services(pool);
+    await services.addUsersOrIncrement('Phindi', 'Goeie Dag');
+    await services.addUsersOrIncrement('Zee', 'Molo');
+    await services.addUsersOrIncrement('Amanda', 'Molo');
+    let results = await services.selectAlldata();
     assert.strictEqual(results.length, 3);
   });
 
 it('Should return the counter value of people counted ', async function() {
-    var Services = services(pool);
-    await Services.addUsersOrIncrement('Phindi','Molo');
-    await Services.addUsersOrIncrement( 'lihle','Molo');
-    await Services.addUsersOrIncrement('Phindi','Molo');
-    let results = await Services.counting();
+    var services = Services(pool);
+    await services.addUsersOrIncrement('Phindi','Molo');
+    await services.addUsersOrIncrement( 'lihle','Molo');
+    await services.addUsersOrIncrement('Phindi','Molo');
+    let results = await services.counting();
     assert.strictEqual(results,2);
   });
 
   it('should return the name of a user selected', async function() {
-      var Services = services(pool);
-      await Services.addUsersOrIncrement('Amanda','Molo');
-      await Services.addUsersOrIncrement( 'Zee','Molo');
-      await Services.addUsersOrIncrement('Zee','Molo');
-      let results = await Services.selectUsers('Zee');
+      var services = Services(pool);
+      await services.addUsersOrIncrement('Amanda','Molo');
+      await services.addUsersOrIncrement( 'Zee','Molo');
+      await services.addUsersOrIncrement('Zee','Molo');
+      let results = await services.selectUsers('Zee');
       assert.strictEqual(results[0].users_greeted, 'Zee');
     });
 
     it('should return the the language of a user selected', async function() {
-        var Services = services(pool);
-        await Services.addUsersOrIncrement('Amanda','Molo');
-        await Services.addUsersOrIncrement( 'Zee','Molo');
-        await Services.addUsersOrIncrement('Zee','Molo');
-        let results = await Services.selectUsers('Zee');
+        var services = Services(pool);
+        await services.addUsersOrIncrement('Amanda','Molo');
+        await services.addUsersOrIncrement( 'Zee','Molo');
+        await services.addUsersOrIncrement('Zee','Molo');
+        let results = await services.selectUsers('Zee');
         assert.strictEqual(results[0].user_language, 'Molo');
       });
   after(function() {
